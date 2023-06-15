4ed2dff942b3503631cd67d811df46d19ed0be05
shervin-vakili <shervin.vakili@inrs.ca>
2023-06-15 18:11:23 -0400

    Emulated function for the second version of the approximation (with 2's complement of mantissa making it unable to handle -128) was added as Approx_Multiply_v2.

--- Python_Emulator/Emulated_Approx_Functions.py
+++ Python_Emulator/Emulated_Approx_Functions.py
@@ -20,15 +20,44 @@
 
 import numpy as np
 
-def Approx_Multiply(multOp1, multOp2):
+def Approx_Multiply_v1(multOp1, multOp2):
     '''
     The first operand (multOp1) is first converted to (2,5,1) float, Mantissa is multiplied by multOp2.
     The result is then converted back to fixed point.
     '''
     multOp1Abs = np.abs(multOp1)
-    #if multOp1 < 0:
-    #  multOp1Abs = multOp1Abs - 1
+    if multOp1 < 0:
+      multOp1Abs = multOp1Abs - 1
     # Encoding to floating point
+    if multOp1Abs == 0:
+        exponent = 0
+    else:
+        exponent = np.floor(np.log2(multOp1Abs)) - 4
+        if exponent < 0:
+            exponent = 0
+
+    mantissa = np.floor(multOp1Abs / pow(2,exponent))  # 5-bit mantissa
+        
+    #print(exponent, mantissa)
+    # Multiplying the Mantissa
+    resMantissa = mantissa * np.abs(multOp2) / 128
+    # Decoder: back to fixed point
+    result = round(resMantissa * pow(2, exponent))
+    # Fixing sign of the result
+    if multOp1 * multOp2 < 0:
+        result = -result
+
+    #print(f'\nOperand1: {multOp1}   exponent: {exponent},   mantissa: {mantissa},    resMantissa = {resMantissa}    Operand2: {multOp2},    result: {result}\n')
+
+    return result 
+
+def Approx_Multiply_v2(multOp1, multOp2):
+    '''
+    The first operand (multOp1) is first converted to (2,5,1) float, Mantissa is multiplied by multOp2.
+    The result is then converted back to fixed point.
+    '''
+    multOp1Abs = np.abs(multOp1)
+
     if multOp1Abs == 0:
         exponent = 0
     else:
@@ -37,16 +66,14 @@ def Approx_Multiply(multOp1, multOp2):
             exponent = 0
     if multOp1 == -128: # Corner case
         mantissa = 0
-    elif multOp1 < 0: 
+    else: 
         mantissa = np.floor(multOp1Abs / pow(2,exponent))  # 5-bit mantissa
-    else:
-        mantissa = np.floor(multOp1Abs / pow(2,exponent))
         
     #print(exponent, mantissa)
     # Multiplying the Mantissa
     resMantissa = round (mantissa * np.abs(multOp2) / 128)
     # Decoder: back to fixed point
-    result = resMantissa * pow(2, exponent)
+    result = round(resMantissa * pow(2, exponent))
     # Fixing sign of the result
     if multOp1 * multOp2 < 0:
         result = -result
